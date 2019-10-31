0b678f10b29b13b9d55831cb91fc8b1983957e14
waltson.silva@hotmail.com <waltson.silva@hotmail.com>
2019-10-30 21:09:03 -0300

    implementação de entidades

--- src/main/java/br/com/help/beans/Profissional.java
+++ src/main/java/br/com/help/beans/Profissional.java
@@ -1,17 +1,13 @@
 package br.com.help.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-import br.com.help.enums.Servicos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,17 +23,7 @@ import lombok.NoArgsConstructor;
 public class Profissional implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false, unique = true, updatable = false)
-
-	private Long id;
-
-	private String cpf;
-
-	private String nome;
-
-	private Servicos servico;
 	
+	private double ranking;
+	private List<Servico> servicos; 
 }
--- src/main/java/br/com/help/beans/Servico.java
+++ src/main/java/br/com/help/beans/Servico.java
@@ -2,7 +2,11 @@ package br.com.help.beans;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -21,6 +25,10 @@ public class Servico implements Serializable{
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, unique = true, updatable = false)
 	private Long id;
 	private String nome;
 
